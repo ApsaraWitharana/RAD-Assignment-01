b622337c98d3bbb0ed8c36c843a8d2248d64a982
ApsaraWitharana <asachini095@gmail.com>
2024-12-27 18:36:53 +0530

    add- dashboard and nav bar update

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,13 @@
-import {useContext, useReducer} from "react";
+import {useContext} from "react";
 import "../style/Dashboard.css"
 import {Customer} from "../model/Customer.ts";
 import {CustomerContext} from "../component/CustomerProvider.tsx";
+import {ItemContext} from "../component/ItemProvider.tsx";
 export function Dashboard() {
 
     const [customers] =useContext(CustomerContext);
+    const [item] =useContext(ItemContext);
+
     return (
         <div className=" md-8">
             <h1 className="text-3xl font-medium text-green-500 mb-6 text-my">Dashboard</h1>
@@ -28,28 +31,27 @@ export function Dashboard() {
                                 <td className="table-css">{customer.address}</td>
                                 <td className="table-css">{customer.email}</td>
                                 <td className="table-css">{customer.phone}</td>
-
                             </tr>
                         ))}
                         </tbody>
                     </table>
                 </div>
-            <div >
-                <div className="p-5">
-                    <h1 className="text-3xl font-medium text-green-500 mb-6">Item</h1>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
-                        {customers.map((customer) => (
-                            <div className="bg-white shadow-md rounded-lg p-4 border border-gray-200">
-                                <p>{customer.name}</p>
-                                <p>{customer.address}</p>
-                                <p>{customer.email}</p>
-                                <p>{customer.phone}</p>
-                            </div>
-                        ))}
+                <div>
+                    <div className="item-card">
+                        <h1 className="text-3xl font-medium text-green-500 mb-6">Item</h1>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+                            {item.map((item) => (
+                                <div className="bg-white shadow-md rounded-lg p-4 border border-gray-200 hover:bg-green-200 transition duration-500">
+                                    <p><span className="font-bold">Code:</span> {item.code}</p>
+                                    <p><span className="font-bold">Name:</span> {item.name}</p>
+                                    <p><span className="font-bold">Quantity:</span> {item.quantity}</p>
+                                    <p><span className="font-bold">Price:</span> {item.price}</p>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 }
